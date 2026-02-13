2393ab26916b568b992dc2b747e8610edf770050
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-02-13 16:02:51 +0530

    adding home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
 import './App.css'
 
 function App() {
@@ -10,8 +11,10 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <Navbar/> 
     <Routes>
-    <Route path="/" element={<Navbar />} />
+ 
+    <Route path="/" element={<Home/>}/>
     </Routes>
 
 

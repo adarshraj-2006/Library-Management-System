2c57fa97725389c3caa8de4abc7736378d493655
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-02-24 23:07:50 +0530

    feat(frontend): hide navbar/footer on login and add toast notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate, useLocation } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Catalog from './pages/Catalog/Catalog'
@@ -11,26 +11,41 @@ import Borrowpage from "./pages/Borrowpage/Borrowpage";
 import Login from "./pages/Login/Login";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import Footer from "./components/Footer/Footer";
+import { Toaster } from 'react-hot-toast';
+
+function Layout({ children }) {
+  const location = useLocation();
+  const hideNavAndFooter = location.pathname === "/Login";
+
+  return (
+    <>
+      {!hideNavAndFooter && <Navbar />}
+      {children}
+      {!hideNavAndFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <ErrorBoundary>
-          <Routes>
-            <Route path="/" element={<Navigate to="/Home" replace />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Catalog" element={<Catalog />} />
-            <Route path="/mybooks" element={<Mybooks />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/borrow" element={<Borrowpage />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </ErrorBoundary>
-        <Footer />
+        <Toaster position="top-center" reverseOrder={false} />
+        <Layout>
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Navigate to="/Home" replace />} />
+              <Route path="/Home" element={<Home />} />
+              <Route path="/Catalog" element={<Catalog />} />
+              <Route path="/mybooks" element={<Mybooks />} />
+              <Route path="/Contact" element={<Contact />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/borrow" element={<Borrowpage />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </ErrorBoundary>
+        </Layout>
       </BrowserRouter>
     </>
   )

85737d5ca683bff942e0bcc1a01398e27dd14caa
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-03-16 11:44:20 +0530

    chore: remove debug marker

--- test_url.js
+++ test_url.js
@@ -8,4 +8,4 @@ console.log("Domain only:", sanitize("https://example.com"));
 console.log("Domain with slash:", sanitize("https://example.com/"));
 console.log("Domain with api:", sanitize("https://example.com/api"));
 console.log("Domain with api slash:", sanitize("https://example.com/api/"));
-console.log("Domain with double api:", sanitize("https://example.com/api/api/"));
+
